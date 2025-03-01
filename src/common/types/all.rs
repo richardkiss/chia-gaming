0de165f9b14c6c28a5774d2f7278265074f34daf
Richard Kiss <him@richardkiss.com>
2025-02-28 21:23:56 -0800

    rc_node

--- src/common/types/all.rs
+++ src/common/types/all.rs
@@ -1,6 +1,4 @@
-use std::borrow::Borrow;
 use std::ops::Add;
-use std::rc::Rc;
 
 use log::debug;
 
@@ -265,25 +263,4 @@ fn test_local_divmod() {
         divmod(7.to_bigint().unwrap(), 2.to_bigint().unwrap()),
         (3.to_bigint().unwrap(), 1.to_bigint().unwrap())
     );
-}
-
-pub struct RcNode<X>(Rc<X>);
-
-impl<E: ClvmEncoder<Node = NodePtr>, X: ToClvm<E>> ToClvm<E> for RcNode<X> {
-    fn to_clvm(&self, encoder: &mut E) -> Result<<E as ClvmEncoder>::Node, ToClvmError> {
-        let borrowed: &X = self.0.borrow();
-        borrowed.to_clvm(encoder)
-    }
-}
-
-impl<X> RcNode<X> {
-    pub fn new(node: Rc<X>) -> Self {
-        RcNode(node.clone())
-    }
-}
-
-impl<X> From<&Rc<X>> for RcNode<X> {
-    fn from(item: &Rc<X>) -> RcNode<X> {
-        RcNode(item.clone())
-    }
 }
--- src/common/types/mod.rs
+++ src/common/types/mod.rs
@@ -5,6 +5,7 @@ pub mod error;
 pub mod game_id;
 pub mod private_key;
 pub mod program;
+pub mod rc_node;
 
 pub use self::all::*;
 pub use self::coin_id::*;
@@ -13,3 +14,4 @@ pub use self::error::*;
 pub use self::game_id::*;
 pub use self::private_key::*;
 pub use self::program::*;
+pub use self::rc_node::*;
